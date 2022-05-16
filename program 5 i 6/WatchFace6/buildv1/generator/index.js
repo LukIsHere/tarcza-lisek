dc4f271ae6607b6361921a7855c7eb62b03f0156
Your Name <luktvpl@wp.pl>
2022-05-16 16:56:08 +0200

    mini update

---
+++ program 5 i 6/WatchFace6/buildv1/generator/index.js
@@ -0,0 +1,7 @@
+
+for (var  i = 0; i < 15; i++) {
+    var inff  = 16*i;
+    var bon = 42+inff;
+    console.log('{"AnimationImages": {"X": '+inff+',"Y": 166,"ImageIndex": '+bon+',"ImagesCount": 64},"Speed": 100,"RepeatCount": 10,"UnknownTF4": 1},')
+    
+}
